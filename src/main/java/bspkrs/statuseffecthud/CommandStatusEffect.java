22f033ae53b7129c06120a4fde5fdce2ffb4bdff
Matjaz Rihtar <matjaz@eunet.si>
2016-12-21 19:37:59 +0100

    1.11 update

--- src/main/java/bspkrs/statuseffecthud/CommandStatusEffect.java
+++ src/main/java/bspkrs/statuseffecthud/CommandStatusEffect.java
@@ -3,6 +3,7 @@ package bspkrs.statuseffecthud;
 import bspkrs.fml.util.DelayedGuiDisplayTicker;
 import bspkrs.statuseffecthud.fml.gui.GuiSEHConfig;
 import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommand;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.server.MinecraftServer;
@@ -10,19 +11,19 @@ import net.minecraft.server.MinecraftServer;
 public class CommandStatusEffect extends CommandBase
 {
     @Override
-    public String getCommandName()
+    public String getName()
     {
         return "statuseffect";
     }
 
     @Override
-    public String getCommandUsage(ICommandSender var1)
+    public String getUsage(ICommandSender var1)
     {
         return "commands.statuseffect.usage";
     }
 
     @Override
-    public boolean checkPermission(MinecraftServer server, ICommandSender par1ICommandSender)
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender)
     {
         return true;
     }
@@ -34,7 +35,7 @@ public class CommandStatusEffect extends CommandBase
     }
 
     @Override
-    public void execute(MinecraftServer server, ICommandSender var1, String[] var2)
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException
     {
         try
         {
@@ -50,7 +51,7 @@ public class CommandStatusEffect extends CommandBase
     public int compareTo(ICommand object)
     {
         if (object instanceof CommandBase)
-            return this.getCommandName().compareTo(((CommandBase) object).getCommandName());
+            return this.getName().compareTo(((CommandBase) object).getName());
 
         return 0;
     }
--- src/main/java/bspkrs/statuseffecthud/fml/Reference.java
+++ src/main/java/bspkrs/statuseffecthud/fml/Reference.java
@@ -4,9 +4,9 @@ import net.minecraftforge.common.config.Configuration;
 
 public class Reference
 {
-    public static final String  MODID        = "StatusEffectHUD";
+    public static final String  MODID        = "statuseffecthud";
     public static final String  NAME         = "StatusEffectHUD";
-    public static final String  VERSION      = "1.40";
+    public static final String  VERSION      = "1.41";
     public static final String  PROXY_COMMON = "bspkrs.statuseffecthud.fml.CommonProxy";
     public static final String  PROXY_CLIENT = "bspkrs.statuseffecthud.fml.ClientProxy";
     public static final String  GUI_FACTORY  = "bspkrs.statuseffecthud.fml.gui.ModGuiFactoryHandler";
